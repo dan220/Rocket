5762abdcaf24f843aa6e80bc27438c9eb762e6b4
Daniel Storer <danielstorer54@gmail.com>
2022-05-29 11:49:38 +0100

    Added osciallator to object

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+
+     float movementFactor;
+    // Start is called before the first frame update
+
+    [SerializeField] float period = 2f;
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSineWave = 3 * Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSineWave + 1f) / 2f;
+
+        Vector3 offset = movementVector + new Vector3(movementFactor, movementFactor, 0);
+        Vector3 endPosition = startingPosition + offset;
+        transform.position = endPosition;
+    }
+}
